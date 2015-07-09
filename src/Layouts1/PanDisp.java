8b9646562605e211b95e8ebca8afd14cafa26e69
MrGFHCI <scott.grondin@gmail.com>
2015-07-09 14:47:49 -0400

    Space3: shoot the enemy.

---
+++ src/Layouts1/PanDisp.java
@@ -0,0 +1,24 @@
+package Layouts1;
+
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PanDisp extends JPanel // panel definition
+{
+    private JLabel lblName;
+    private String sLabel;
+
+    public PanDisp() {
+        lblName = new JLabel(" Barney");
+        add(lblName); //add it to the Frame
+    }
+
+    void UpdateLabel(String _sNew) {
+        sLabel = _sNew;
+        lblName.setText(sLabel);
+    }    
+}
